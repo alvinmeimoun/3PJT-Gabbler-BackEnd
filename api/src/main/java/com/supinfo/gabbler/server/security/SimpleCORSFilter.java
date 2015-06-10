06cc802bcbe22418d9ccb9a6c6f5e5a86087776d
Alvin Meimoun <alvin.meimoun@gmail.com>
2015-06-10 15:43:09 +0200

    Test CORS *

--- api/src/main/java/com/supinfo/gabbler/server/security/SimpleCORSFilter.java
+++ api/src/main/java/com/supinfo/gabbler/server/security/SimpleCORSFilter.java
@@ -11,7 +11,7 @@ public class SimpleCORSFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://nodejs_n1.gabbler.net:3000");
+        response.setHeader("Access-Control-Allow-Origin", "*"); //http://nodejs_n1.gabbler.net:3000
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, sessionAuthToken");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "POST, GET, PUT, OPTIONS, DELETE, Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With, sessionAuthToken");
